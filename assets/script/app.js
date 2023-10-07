ad825cfbfb35ecf2ccea103b0f37e28bfacf07d9
zaid-kh <z.kh2800@gmail.com>
2023-10-07 09:04:04 +0300

    basic game logic of rolling dice and keeping score

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,7 +1,9 @@
-const score1 = 0; // track player1's score
-let target = 50; // target score
+let score1 = 0; // track player1's score
+let score2 = 0; // track player1's score
+let target = 100; // target score
 let die1 = 0;
 let die2 = 0;
+console.log("hello");
 
 function reloadPage() {
   window.location.reload();
@@ -19,3 +21,33 @@ function isDouble() {
   // todo: add fun thing to show player who got a double
   return false;
 }
+
+const rollBtn = document.querySelector("#roll");
+const holdBtn = document.querySelector("#hold");
+const score1Span = document.querySelector("score1");
+/**
+ * 1 roll dice + check double
+ * 2 add to score
+ * 3 compare with target
+ * 4
+ */
+function rollDice() {
+  if (score1 < target /*|| score2 <target*/) {
+    die1 = rollDie(die1);
+    console.log("rolled die1: ", die1);
+    die2 = rollDie(die2);
+    console.log("rolled die2: ", die2);
+    if (isDouble()) {
+      console.log("isDouble: ", isDouble());
+      score1 = 0;
+      //! track the correct score
+    } else {
+      score1 += die1 + die2;
+      if (score1 >= target) {
+        alert("You won! ");
+      }
+    }
+    console.log("score1: ", score1);
+  }
+}
+rollBtn.addEventListener("click", rollDice);
