20dcd1e8c9873b36e7b686bc2b39a68b30e15a93
zaid-kh <z.kh2800@gmail.com>
2023-10-07 18:54:45 +0300

    Game functions as a 2-player game now

--- assets/script/app.js
+++ assets/script/app.js
@@ -3,7 +3,7 @@ let score2 = 0; // track player1's score
 let target = 100; // target score
 let die1 = 0;
 let die2 = 0;
-console.log("hello");
+let currentPlayer = 1;
 
 function reloadPage() {
   window.location.reload();
@@ -12,42 +12,59 @@ function resetDice() {
   die1 = 0;
   die2 = 0;
 }
-function rollDie(die) {
-  die = Math.floor(Math.random() * 6) + 1;
-  return die;
+
+function rollDie() {
+  return Math.floor(Math.random() * 6) + 1;
 }
-function isDouble() {
-  if (die1 === die2) return true;
-  // todo: add fun thing to show player who got a double
-  return false;
+function switchPlayer() {
+  currentPlayer = currentPlayer === 1 ? 2 : 1;
+  resetDice();
+}
+
+function rollDice() {
+  // prevent action if either player hit the target
+  if (
+    (currentPlayer === 1 && score1 < target) ||
+    (currentPlayer === 2 && score2 < target)
+  ) {
+    die1 = rollDie();
+    console.log(`Player ${currentPlayer} rolled die1: `, die1);
+    die2 = rollDie();
+    console.log(`Player ${currentPlayer} rolled die2: `, die2);
+
+    if (isDouble()) {
+      console.log(`Player ${currentPlayer} got a double!`);
+      currentPlayer === 1 ? (score1 = 0) : (score2 = 0);
+      switchPlayer();
+    } else {
+      currentPlayer === 1 ? (score1 += die1 + die2) : (score2 += die1 + die2);
+      console.log(
+        `Player ${currentPlayer} current score: `,
+        currentPlayer === 1 ? score1 : score2
+      );
+
+      score1Span.textContent = score1;
+      score2Span.textContent = score2;
+      if (score1 > target || score2 > target)
+        alert(`Player ${currentPlayer} Won !`);
+    }
+  }
 }
 
 const rollBtn = document.querySelector("#roll");
 const holdBtn = document.querySelector("#hold");
-const score1Span = document.querySelector("score1");
-/**
- * 1 roll dice + check double
- * 2 add to score
- * 3 compare with target
- * 4
- */
-function rollDice() {
-  if (score1 < target /*|| score2 <target*/) {
-    die1 = rollDie(die1);
-    console.log("rolled die1: ", die1);
-    die2 = rollDie(die2);
-    console.log("rolled die2: ", die2);
-    if (isDouble()) {
-      console.log("isDouble: ", isDouble());
-      score1 = 0;
-      //! track the correct score
-    } else {
-      score1 += die1 + die2;
-      if (score1 >= target) {
-        alert("You won! ");
-      }
-    }
-    console.log("score1: ", score1);
-  }
-}
+const score1Span = document.querySelector("#score1");
+const score2Span = document.querySelector("#score2");
+
 rollBtn.addEventListener("click", rollDice);
+
+function holdListener() {
+  //todo: implement style changes
+  switchPlayer();
+}
+
+holdBtn.addEventListener("click", holdListener);
+
+function isDouble() {
+  return die1 === die2;
+}
